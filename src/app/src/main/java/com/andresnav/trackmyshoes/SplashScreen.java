4ed2fdea7771171f5d5f243f897f43ccff638070
Andres Navarro <contact@andresnav.com>
2024-03-21 11:06:04 +0100

    add: logo

--- src/app/src/main/java/com/andresnav/trackmyshoes/SplashScreen.java
+++ src/app/src/main/java/com/andresnav/trackmyshoes/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.andresnav.trackmyshoes;
 
+import static com.andresnav.trackmyshoes.utils.Utils.print;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -19,15 +21,10 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Log.d("test", "logg" + FirebaseUtil.isLoggedIn());
-                Log.d("test", "user id" + FirebaseUtil.currentUserId());
-
                 if(FirebaseUtil.isLoggedIn()){
-                    Log.d("test", "login true");
                     startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 }
                 else {
-                    Log.d("test", "login false");
                     startActivity(new Intent(SplashScreen.this, LoginActivity.class));
                 }
                 finish();
