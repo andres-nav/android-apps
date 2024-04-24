be8b563118218c4ea5cc9cdd83406865fd666715
Andres Navarro <contact@andresnav.com>
2024-04-24 18:44:15 +0200

    tracking: simple setup to track location

--- src/app/src/main/java/com/andresnav/trackmyshoes/TrackActivity.java
+++ src/app/src/main/java/com/andresnav/trackmyshoes/TrackActivity.java
@@ -2,58 +2,254 @@ package com.andresnav.trackmyshoes;
 
 import static com.andresnav.trackmyshoes.utils.Utils.print;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.lifecycle.Observer;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.andresnav.trackmyshoes.utils.LocationProvider;
-import com.andresnav.trackmyshoes.utils.PermissionManager;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.andresnav.trackmyshoes.utils.LocationService;
 
-public class TrackActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class TrackActivity extends AppCompatActivity {
 
-    private SupportMapFragment map;
+    private LocationService.LocationServiceBinder locationService;
 
-    private LocationProvider locationProvider;
-    private PermissionManager permissionManager;
+    private TextView textViewDistanceRun, textViewDuration, textViewAvgSpeed;
+    private Button buttonStart, buttonStop, buttonSave;
+
+    private static final int PERMISSION_GPS_CODE = 1;
+
+    // will poll the location service for distance and duration
+    private Handler postBack = new Handler();
+
+    private ServiceConnection lsc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            locationService = (LocationService.LocationServiceBinder) iBinder;
+
+            // if currently tracking then enable stopButton and disable startButton
+            print("service connected");
+            initButtons();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (locationService != null) {
+                        // get the distance and duration from the surface
+                        float d = (float) locationService.getDuration();
+                        long duration = (long) d;  // in seconds
+                        float distance = locationService.getDistance();
+
+                        long hours = duration / 3600;
+                        long minutes = (duration % 3600) / 60;
+                        long seconds = duration % 60;
+
+                        float avgSpeed = 0;
+                        if(d != 0) {
+                            avgSpeed = distance / (d / 3600);
+                        }
+
+                        final String time = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+                        final String dist = String.format("%.2f KM", distance);
+                        final String avgs = String.format("%.2f KM/H", avgSpeed);
+
+                        postBack.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                // post back changes to UI thread
+                                textViewDuration.setText(time);
+                                textViewAvgSpeed.setText(avgs);
+                                textViewDistanceRun.setText(dist);
+                            }
+                        });
+
+                        try {
+                            Thread.sleep(500);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            locationService = null;
+        }
+    };
+
+    // whenever activity is reloaded while still tracking a journey (if back button is clicked for example)
+    private void initButtons() {
+        // no permissions means no buttons
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            buttonStop.setEnabled(false);
+            buttonStart.setEnabled(false);
+            buttonSave.setEnabled(false);
+            return;
+        }
+
+        // if currently tracking then enable stopButton and disable startButton
+        if(locationService != null && locationService.currentlyTracking()) {
+            buttonStop.setEnabled(true);
+            buttonStart.setEnabled(false);
+            buttonSave.setEnabled(false);
+        } else {
+            buttonStop.setEnabled(false);
+            buttonStart.setEnabled(true);
+            buttonSave.setEnabled(true);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
-        locationProvider = new LocationProvider(this);
-        permissionManager = new PermissionManager(this, locationProvider);
 
-        map = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapView);
-        map.getMapAsync(this);
-    }
+        textViewDistanceRun = findViewById(R.id.textViewDistanceRun);
+        textViewDuration = findViewById(R.id.textViewDuration);
+        textViewAvgSpeed = findViewById(R.id.textViewAvgSpeed);
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        locationProvider.getLiveLocation().observe(this, new Observer<LatLng>() {
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonSave = findViewById(R.id.buttonSave);
+
+        buttonStart.setEnabled(false);
+        buttonStop.setEnabled(false);
+        buttonSave.setEnabled(false);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(LatLng latLng) {
-                print("moveedd");
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f));
+            public void onClick(View v) {
+                locationService.playJourney();
+                buttonStart.setEnabled(false);
+                buttonStop.setEnabled(true);
             }
         });
 
-        permissionManager.requestUserLocation();
+        handlePermissions();
 
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.setMyLocationEnabled(true);
+        startService(new Intent(this, LocationService.class));
+        bindService(
+                new Intent(this, LocationService.class), lsc, Context.BIND_AUTO_CREATE);
+
+    }
+
+    public void onClickStop(View view) {
+        // save the current journey to the database
+        float distance = locationService.getDistance();
+        locationService.saveJourney();
+
+        buttonStart.setEnabled(false);
+        buttonStop.setEnabled(false);
+
+        DialogFragment modal = FinishedTrackingDialogue.newInstance(String.format("%.2f KM", distance));
+        modal.show(getSupportFragmentManager(), "Finished");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // unbind to the service (if we are the only binding activity then the service will be destroyed)
+        if(lsc != null) {
+            unbindService(lsc);
+            lsc = null;
+        }
+    }
+
+    public static class FinishedTrackingDialogue extends DialogFragment {
+        public static  FinishedTrackingDialogue newInstance(String distance) {
+            Bundle savedInstanceState = new Bundle();
+            savedInstanceState.putString("distance", distance);
+            FinishedTrackingDialogue frag = new FinishedTrackingDialogue();
+            frag.setArguments(savedInstanceState);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Your Journey has been saved. You ran a total of " + getArguments().getString("distance") + " KM")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // go back to home screen
+                            getActivity().finish();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+    public static class NoPermissionDialogue extends DialogFragment {
+        public static  NoPermissionDialogue newInstance() {
+            NoPermissionDialogue frag = new NoPermissionDialogue();
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(getString(R.string.dialog_gps))
+                    .setPositiveButton(getString(R.string.text_enable_gps), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // user agreed to enable GPS
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_GPS_CODE);
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.text_cancel), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // dialogue was cancelled
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+    private void handlePermissions() {
+        // if don't have GPS permissions then request this permission from the user.
+        // if not granted the permission disable the start button
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            buttonStop.setEnabled(false);
+            buttonStart.setEnabled(false);
+            buttonSave.setEnabled(false);
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // the user has already declined request to allow GPS
+                // give them a pop up explaining why its needed and re-ask
+                DialogFragment modal = NoPermissionDialogue.newInstance();
+                modal.show(getSupportFragmentManager(), "Permissions");
+            } else {
+                // request the permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_GPS_CODE);
+            }
+        } else {
+            initButtons();
+            if (locationService != null) {
+                locationService.notifyGPSEnabled();
+            }
+        }
     }
 }
--- src/app/src/main/java/com/andresnav/trackmyshoes/utils/LocationProvider.java
+++ src/app/src/main/java/com/andresnav/trackmyshoes/utils/LocationProvider.java
@@ -2,15 +2,20 @@ package com.andresnav.trackmyshoes.utils;
 
 import android.annotation.SuppressLint;
 import android.location.Location;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.android.SphericalUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,18 +28,65 @@ public class LocationProvider {
 
     private final List<LatLng> locations = new ArrayList<>();
 
+    private float distance = 0;
+
     final MutableLiveData<LatLng> liveLocation = new MutableLiveData<>();
+    final MutableLiveData<List<LatLng>> liveLocations = new MutableLiveData<>();
+    final MutableLiveData<Float> liveDistance = new MutableLiveData<>();
+
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult result) {
+            Location currentLocation = result.getLastLocation();
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+            LatLng lastLocation = locations.isEmpty() ? null : locations.get(locations.size() - 1);
+
+            if (lastLocation != null) {
+                distance += Math.round(SphericalUtil.computeDistanceBetween(lastLocation, latLng));
+                liveDistance.setValue(distance);
+            }
+
+            locations.add(latLng);
+            liveLocations.setValue(locations);
+        }
+    };
 
     public LocationProvider(AppCompatActivity activity) {
         this.activity = activity;
         this.client = LocationServices.getFusedLocationProviderClient(activity);
     }
 
+    public MutableLiveData<List<LatLng>> getLiveLocations() {
+        return liveLocations;
+    }
+
     public MutableLiveData<LatLng> getLiveLocation() {
         return liveLocation;
     }
 
-    //4
+    public MutableLiveData<Float> getLiveDistance() {
+        return liveDistance;
+    }
+
+    public void trackUser() {
+        //1
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+
+        //2
+        client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+
+    public void stopTracking() {
+        client.removeLocationUpdates(locationCallback);
+        locations.clear();
+        distance = 0;
+    }
+
     public void getUserLocation() {
         Task<android.location.Location> locationTask = client.getLastLocation();
         locationTask.addOnSuccessListener(activity, new OnSuccessListener<Location>() {
